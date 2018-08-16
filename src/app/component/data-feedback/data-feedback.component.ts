e6d8f17669a46c7b8da60fd4f55d8f8b000a3fca
Pardeep <pk.raswant@gmail.com>
2018-08-17 03:08:51 +0530

    added content download data changes

--- src/app/component/data-feedback/data-feedback.component.ts
+++ src/app/component/data-feedback/data-feedback.component.ts
@@ -8,13 +8,17 @@ import { DatafetchfeedbackService } from '../../services/datafetchfeedback.servi
   templateUrl: './data-feedback.component.html',
   styleUrls: ['./data-feedback.component.css']
 })
+
+
 export class DataFeedbackComponent implements OnInit {
   private selectedSprint:number;
-  private isShowtableheader:boolean=true;
+  private isHidetableheader:boolean=true;
 
   buildHeaderTable=[];
 
   fetchData_feedback=[];
+  quesIdsGlobal=[];
+  console=console;
 
   sprints = [{
   "id": 1,
@@ -35,6 +39,11 @@ export class DataFeedbackComponent implements OnInit {
   "id": 6,
   "val": "Sprint 6"
 }];
+
+
+
+
+//  formIdsDetails=[];
 
   constructor(private spinner:NgxSpinnerService,private datafetchfeedback:DatafetchfeedbackService) { }
 
@@ -59,6 +68,7 @@ export class DataFeedbackComponent implements OnInit {
            this.spinner.hide();
           // console.log(data);
           this.buildCustomHeader(data);
+          this.buildTableBody(data);
          }
          else{
            this.spinner.hide();
@@ -93,6 +103,7 @@ export class DataFeedbackComponent implements OnInit {
     let feedbackdetObj=data.fb_detail_data_group_formid;
     let detObj=feedbackdetObj[formidFirst];
     let menteeNames=new Array();
+    let menteeDetails=new Array();
 
     for(var i=0;i<detObj.length;i++){
       let childObj=detObj[i];
@@ -100,17 +111,109 @@ export class DataFeedbackComponent implements OnInit {
 
       if(menteeNames.indexOf(childObj.mentee_name.toString().trim())==-1){
       menteeNames.push(childObj.mentee_name.toString().trim());
+      // get all unique mentee and put in header part
+        this.buildHeaderTable.push({
+          "name":childObj.mentee_name.toString().trim(),
+          "mentee_id":childObj.mentee_id.toString().trim()
+        });
     }
 
     }
 
-    console.log(menteeNames);
+    //console.log(menteeNames);
+    this.isHidetableheader=false;
 
-    
+    // for(var i=0;i<menteeNames.length;i++){
+    //   this.buildHeaderTable.push({
+    //     "name":menteeNames[i]
+    //   });
+    // }
+
+
   }
   catch(except){
     console.log(except);
   }
+  }
+
+  buildTableBody(data){
+    try{
+      let quesids=[];
+      let quesIdData=[];
+      let menteeDetails=[];
+      let feedbackDateData=[];
+        // step first get all form ids and there program names
+        let sprintFeedbackIds=data.sprint_cyc;
+
+        for(var i=0;i<sprintFeedbackIds.length;i++){
+          let mform_id=""+sprintFeedbackIds[i].form_id;
+          let mprg_id=""+sprintFeedbackIds[i].prg_id;
+          let feedbackData=data.fb_detail_data_group_formid;
+          //console.log("feedback data"+feedbackData);
+          var formIdData=feedbackData[mform_id];
+          //console.log("form "+mform_id);
+          //console.log(mprg_id);
+          //console.log("feedback data"+JSON.stringify(data));
+
+          //console.log(formIdData);
+
+          quesids=new Array();
+          quesIdData=new Array();
+          for(var j=0;j<formIdData.length;j++){
+            if(quesids.indexOf(formIdData[j].ques_id)==-1){
+              quesids.push(formIdData[j].ques_id);
+          //    console.log("ques id :"+formIdData[j].ques_id);
+              quesIdData.push({
+                "ques_id":""+formIdData[j].ques_id,
+                "question":formIdData[j].question
+              });
+            }
+          }
+        //  console.log("question Id Data :"+quesIdData[0].ques_id);
+          // get mentee Details
+          let feedback_detail_data=data.fb_detail_data_group_qid;
+          //console.log("feedbackdetail data "+feedback_detail_data);
+          menteeDetails=new Array();
+          menteeDetails=feedback_detail_data[mform_id];
+
+          let sharedateData=data.feedback_share_date_data;
+          if(mform_id in sharedateData){
+            feedbackDateData=new Array();
+            feedbackDateData=sharedateData[mform_id];
+          }
+          else{
+            feedbackDateData=null;
+          }
+
+          //get program name
+          let prgData=data.prg_data;
+          let prgDataObj=prgData[mprg_id];
+        //  console.log("prg data"+prgDataObj[0].prg_name);
+
+
+
+
+
+
+          this.fetchData_feedback.push({
+            "form_id":mform_id,
+            "prg_id":mprg_id,
+            "prg_name":prgDataObj[0].prg_name.toString().trim(),
+            "feedbacksharedate_data":feedbackDateData,
+            "questions_data":quesIdData,
+            "menteeData":menteeDetails
+          })
+        }
+        console.log(this.fetchData_feedback);
+
+        // for(var i=0;i<this.fetchData_feedback.length;i++){
+        //
+        // }
+
+    }
+    catch(exception){
+      console.log(exception);
+    }
   }
 
 }
